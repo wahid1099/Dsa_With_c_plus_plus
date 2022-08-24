133fae390cfaa30bc325bf09608edb9d3657ffe0
wahid1099 <wahidahmed890@gmail.com>
2022-08-24 20:14:53 +0600

    adding

--- Module24Final/Question7.cpp
+++ Module24Final/Question7.cpp
@@ -2,55 +2,68 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-class Node{
-     public:
+class Node
+{
+public:
     Node *next;
     int value;
 
-    Node(int val){
+    Node(int val)
+    {
         value=val;
         next=NULL;
     }
 };
-void display(Node *n){
+void display(Node *n)
+{
 
-    while(n != NULL){
+    while(n != NULL)
+    {
         cout<<n->value;
-        if(n->next!=NULL){
+        if(n->next!=NULL)
+        {
             cout<<" -> ";
         }
         n=n->next;
     }
 }
 
-void insertAttail(Node *&head,int val){
+void insertAttail(Node *&head,int val)
+{
     Node *newNode=new Node(val);
 
-    if (head==NULL){
+    if (head==NULL)
+    {
         head=newNode;
         return;
-        }
-        Node *temp=head;
-        while(temp->next != NULL){
-            temp=temp->next;
-        }
-        temp->next=newNode;
+    }
+    Node *temp=head;
+    while(temp->next != NULL)
+    {
+        temp=temp->next;
+    }
+    temp->next=newNode;
 }
 
-Node *reverseKthNode(Node *&head,int k){
+Node *reverseKthNode(Node *&head,int k)
+{
+
+    // 1 2 3 4 5 6  7 8
     Node *prevptr=NULL;
     Node *currentptr=head;
     Node *nextptr;
     int count=0;
-    while(currentptr!=NULL && count<k){
+    while(currentptr!=NULL && count<k)
+    {
         nextptr=currentptr->next;
         currentptr->next=prevptr;
         prevptr=currentptr;
         currentptr=nextptr;
-         count++;
+        count++;
     }
 
-    if(nextptr!=NULL){
+    if(nextptr!=NULL)
+    {
         head->next=reverseKthNode(nextptr,k);
     }
     return prevptr;
@@ -58,29 +71,30 @@ Node *reverseKthNode(Node *&head,int k){
 }
 int main()
 {
- int n,k,value;
- Node *head=NULL;
- cout<<"How many elements do you want to add :";
-cin>>n;
-cout<<endl;
-cout<<"Enter linked list elements: ";
+    int n,k,value;
+    Node *head=NULL;
+    cout<<"How many elements do you want to add :";
+    cin>>n;
+    cout<<endl;
+    cout<<"Enter linked list elements: ";
 
- for(int i=0;i< n; i++){
-    cin >>value;
-    insertAttail(head,value);
- }
- cout<<"Linked list before reversing: ";
- display(head);
- cout<<endl;
-cout<<"Enter the kth position to reverse: ";
-cin>>k;
- cout<<endl;
+    for(int i=0; i< n; i++)
+    {
+        cin >>value;
+        insertAttail(head,value);
+    }
+    cout<<"Linked list before reversing: ";
+    display(head);
+    cout<<endl;
+    cout<<"Enter the kth position to reverse: ";
+    cin>>k;
+    cout<<endl;
 
-Node *newhead=reverseKthNode(head,k);
-  cout<<"Linked list after reversing: ";
- display(newhead);
+    Node *newhead=reverseKthNode(head,k);
+    cout<<"Linked list after reversing: ";
+    display(newhead);
 
 
 
-  return 0;
+    return 0;
 }
